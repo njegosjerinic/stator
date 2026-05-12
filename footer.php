d5cdcd918e353f6af5309b7cf667bc3c0ea6dc67
Njegos Jerinic <njegos.jerinic@gmail.com>
2026-05-12 17:22:07 +0200

    Contact Page desing

--- footer.php
+++ footer.php
@@ -1,14 +1,21 @@
 <footer class="footer">
   <div class="footer-column">
-    <h4>📍 Lokacija</h4>
-    <p>Ulica Vojvode Mišića bb<br>74000 Doboj, Bosna i Hercegovina</p>
+    <h4>Lokacija</h4>
+
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d840.483753512222!2d18.09196242797154!3d44.735714812731935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475e839d2ec567f1%3A0x587a2fc317f13873!2sStator%20GS!5e1!3m2!1sbs!2sba!4v1778592658836!5m2!1sbs!2sba"
+      width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    <p>Ulica Vojvode Mišića bb</p>
+    <p>74000 Doboj, Bosna i Hercegovina</p>
   </div>
 
   <div class="footer-column">
-    <h4>☎️ Kontakt</h4>
+    <h4>Kontakt</h4>
     <ul>
-      <li><strong>Servis i Prodavnica:</strong> +387 53 226 383</li>
       <li><strong>Direktor:</strong> +387 65 323 324</li>
+      <li><strong>Servis i Prodavnica:</strong> +387 53 226 383</li>
       <li><strong>Šef servisa:</strong> +387 65 715 085</li>
       <li><strong>Komercijala:</strong> +387 66 900 112</li>
       <li><strong>Prodaja stanova:</strong> +387 66 333 308</li>
@@ -16,13 +23,13 @@
   </div>
 
   <div class="footer-column">
-    <h4>✉️ Email</h4>
+    <h4>Email</h4>
     <p>
-      <a style="padding:5px;" href="mailto:komercijala@statorgs.com">komercijala@statorgs.com</a>
-	  <a style="padding:5px;" href="mailto:energetika@statorgs.com">energetika@statorgs.com</a>
-	  <a style="padding:5px;" href="mailto:bakarna.zica@statorgs.com">bakarna.zica@statorgs.com</a>
-      <a style="padding:5px;" href="mailto:prodajastanova@statorgs.com">prodajastanova@statorgs.com</a>
-      <a style="padding:5px;" href="mailto:finansije@statorgs.com">finansije@statorgs.com</a>
+      <a href="mailto:komercijala@statorgs.com">komercijala@statorgs.com</a>
+      <a href="mailto:energetika@statorgs.com">energetika@statorgs.com</a>
+      <a href="mailto:bakarna.zica@statorgs.com">bakarna.zica@statorgs.com</a>
+      <a href="mailto:prodajastanova@statorgs.com">prodajastanova@statorgs.com</a>
+      <a href="mailto:finansije@statorgs.com">finansije@statorgs.com</a>
     </p>
   </div>
 </footer>
